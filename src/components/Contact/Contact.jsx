42e9d3a82e9dcc2c7de1c74cfe6a523e10da7d55
Atorami <atoramiseo@mail.ru>
2024-02-04 21:31:16 +0100

    feat: add contact info

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -7,16 +7,96 @@ const Contact = () => {
     <section className="bg-gray-100 h-[700px]" id="contact">
       <div className="container mx-auto">
         <h2 className="text-3xl lg:text-4xl font-extrabold font-popponi text-left pt-14">
-          <span className="text-6xl text-gray-400 border-2 p-1">C</span>ontact
+          <span className="text-6xl text-gray-400 border-2 p-1">G</span>et in
+          touch!
         </h2>
         <div className="flex px-16 py-10">
-          {/* Left Side - Contact Form */}
           <div className="w-1/2">
             <ContactForm />
           </div>
-
-          {/* Right Side - Map */}
           <div className="w-1/2">
+            <div className="h-[150px] flex flex-row justify-between bg-white py-10 px-4">
+              <div className="">
+                <div className="flex flex-row">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6 mr-2"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                    />
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z"
+                    />
+                  </svg>
+                  <h5 className="font-bold text-xl mb-4">Adress:</h5>
+                </div>
+
+                <p>
+                  st. Name 21/43b
+                  <br /> Szczecin, 71-222
+                </p>
+              </div>
+              <div className="">
+                <div className="flex flex-row">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6 mr-4"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z"
+                    />
+                  </svg>
+
+                  <h5 className="font-bold text-xl mb-4">Phone:</h5>
+                </div>
+
+                <p>
+                  Mobile 1: <a href="tel:+4888888888">+(48)888-888-888</a>
+                  <br />
+                  Mobile 2: <a href="tel:+4888888889">+(48)888-888-889</a>
+                </p>
+              </div>
+              <div className="mr-10">
+                <div className="flex flex-row">
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-6 h-6 mr-4"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75"
+                    />
+                  </svg>
+                  <h5 className="font-bold text-xl mb-4">Email:</h5>
+                </div>
+
+                <p>
+                  <a href="mailto:voltron@voltron.com.pl">
+                    voltron@voltron.com.pl
+                  </a>
+                </p>
+              </div>
+            </div>
             <Map />
           </div>
         </div>
--- src/components/Contact/ContactComponents/Map/Map.jsx
+++ src/components/Contact/ContactComponents/Map/Map.jsx
@@ -6,7 +6,7 @@ const Map = () => {
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d19018.27583330528!2d14.525169799999999!3d53.4276216!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2spl!4v1706601958869!5m2!1sen!2spl"
         width="100%"
-        height="500"
+        height="350"
         style={{ border: "0" }}
         allowFullScreen=""
         loading="lazy"
