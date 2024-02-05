999277507c1c50d120b86c3ca14606997f87bc33
Atorami <atoramiseo@mail.ru>
2024-02-05 20:24:47 +0100

    fix: adopt footer fix

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,14 +5,14 @@ const Footer = () => {
   return (
     <footer className="bg-gray-700 text-white py-6">
       <div className="container mx-auto">
-        <div className="flex flex-col items-center lg:items-start">
+        <div className="flex flex-col items-center lg:items-baseline ">
           <div className="w-40">
             <a href="">
               {" "}
               <img src={logo} alt="Voltron" />
             </a>
           </div>
-          <div className="flex flex-col lg:flex-row lg:justify-between items-center pt-4">
+          <div className="w-full flex flex-col xl:flex-row xl:justify-between items-center pt-4">
             <p>© 2024 Voltron. All rights reserved.</p>
             <p>
               Created by <a href="#"> AM </a>
